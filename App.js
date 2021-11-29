3e93e4646d7541f7a5b86c1e57513c109acecb62
chinmay <chinmaymhatre12@gmail.com>
2021-11-29 11:46:53 +0530

    speech

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [text, setText] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <TouchableOpacity
+        
+        style={styles.button}
+        onPress={() => {
+          Speech.speak(text);
+        }}
+      ><Text style={styles.buttonText}>Speak</Text></TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +32,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+    width: '80%'
+  },
+  buttonText:{
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  button:{
+    backgroundColor: '#ccc',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    margin: 10,
+
+  }
 });
